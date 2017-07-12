9945a53ae2bd5190df38d1b946e9736a48ee7bd9
michael <michael.pc168@gmail.com>
2017-07-12 23:12:25 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Easter
+
+Given a year, finds out the date of Easter Sunday for that year.  
+Utilises formula at https://www.drupal.org/node/1180480
+
+## Usage
+
+```bash
+$ gcc easter.c -o easter
+$ ./easter
+Enter Year: 2017
+Easter is April 16 in 2017
+```
+
+## Installation
+
+Compile with `gcc easter -o easter`
+
+## Contributing
+
+Do as you wish, I suppose
+
+## License
+
+MIT
---
+++ easter.c
@@ -0,0 +1,74 @@
+//name: michael
+//date: 20170306
+//find easter date
+
+#include <stdio.h>
+
+
+int main(void){
+    int year, a, b, c, d, e, f, g, h, i, k, l, m, month, p, date;
+
+    printf("Enter Year: ");
+    scanf("%d", &year);
+
+    a=year%19;
+    b=year/100;
+    c=year%100;
+    d=b/4;
+    e=b%4;
+    f=(b+8)/25;
+    g=(b-f+1)/3;
+    h=(19*a+b-d-g+15)%30;
+    i=c/4;
+    k=c%4;
+    l=(32+2*e+2*i-h-k)%7;
+    m=(a+11*h+22*l)/451;
+    month =(h+l-7*m+114)/31;
+    p=(h+l-7*m+114)%31;
+    date =p+1;
+
+    printf("Easter is ");
+    switch(month){
+        case 1:
+            printf("Janurary");
+            break;
+        case 2:
+            printf("Feburary");
+            break;
+        case 3:
+            printf("March");
+            break;
+        case 4:
+            printf("April");
+            break;
+        case 5:
+            printf("May");
+            break;
+        case 6:
+            printf("June");
+            break;
+        case 7:
+            printf("July");
+            break;
+        case 8:
+            printf("August");
+            break;
+        case 9:
+            printf("September");
+            break;
+        case 10:
+            printf("October");
+            break;
+        case 11:
+            break;
+            printf("November");
+        case 12:
+            printf("December");
+            break;
+        default:
+            printf("What");
+
+    }
+    printf(" %d in %d.\n", date, year);
+
+}
